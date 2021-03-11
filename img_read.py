92ee26e65acde585758c618239b06ff8f9c50495
Shibin Thomas <sshibinthomass@gmail.com>
2021-03-11 22:28:09 +0530

    first

---
+++ img_read.py
@@ -0,0 +1,14 @@
+import cv2
+#read image
+# img=cv2.imread('E:/photo/2.jpg')
+# cv2.imshow('frame',img)
+# cv2.waitKey(0)
+
+capture=cv2.VideoCapture(0)
+while True:
+    a,frame=capture.read()
+    cv2.imshow('Video',frame)
+    if cv2.waitKey(20 & 0xFF==27):
+        break
+capture.release()
+cv2.destroyAllWindows()
